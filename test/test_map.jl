a3479bf4a50e2208818b2607349ed291f0c52775
Takafumi Arakaki <aka.tkf@gmail.com>
2021-01-26 20:29:16 -0500

    A few more tests

--- test/test_map.jl
+++ test/test_map.jl
@@ -6,9 +6,12 @@ include("utils.jl")
 
 @testset begin
     @test Folds.map(identity, 1:10) == 1:10
+    @test Folds.map(identity, reshape(1:6, 2, 3))::Matrix{Int} == reshape(1:6, 2, 3)
     @test Folds.map(tuple, 1:3, 4:6, 7:9) == collect(zip(1:3, 4:6, 7:9))
     @test Folds.map(identity, 1:10, SequentialEx()) == 1:10
     @test Folds.map(tuple, 1:3, 4:6, 7:9, SequentialEx()) == collect(zip(1:3, 4:6, 7:9))
+    @test Folds.map(identity, reshape(1:6, 2, 3), SequentialEx())::Matrix{Int} ==
+          reshape(1:6, 2, 3)
 end
 
 @testset "executor with keyword arguments" begin
