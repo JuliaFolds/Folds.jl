1fdeb9d7ee8a8e0d78c5352152306837721ca1db
Takafumi Arakaki <aka.tkf@gmail.com>
2021-05-29 23:23:47 -0400

    Better testing for compat

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -11,17 +11,7 @@ using Test
     Aqua.test_ambiguities(Folds)
 end
 
-@testset "Unbound type parameters" begin
-    Aqua.test_unbound_args(Folds)
-end
-
-@testset "Undefined exports" begin
-    Aqua.test_undefined_exports(Folds)
-end
-
-@testset "Compare Project.toml and test/Project.toml" begin
-    Aqua.test_project_extras(Folds)
-end
+Aqua.test_all(Folds; ambiguities = false)
 
 @testset "Compare test/Project.toml and test/environments/main/Project.toml" begin
     @test Text(read(joinpath(@__DIR__, "Project.toml"), String)) ==
