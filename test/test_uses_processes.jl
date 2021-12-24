42e3ff91426163219a82c43b55c5a099ee4b2860
Takafumi Arakaki <aka.tkf@gmail.com>
2021-12-24 00:59:49 -0500

    Revert "Don't run `test_uses_processes` in Julia 1.8-DEV"
    This reverts commit 5b7a98cdee46725596454e80b4585d7abfb4c60c.

--- test/test_uses_processes.jl
+++ test/test_uses_processes.jl
@@ -1,6 +1,5 @@
 module TestUsesProcesses
 using Folds
 using Folds.Testing: test_uses_processes
-VERSION < v"1.8-" && # workaround the hang in CI
 test_uses_processes(DistributedEx())
 end  # module
